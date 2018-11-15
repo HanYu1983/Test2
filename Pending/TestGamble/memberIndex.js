dca181f761fa90157b0bce9d59694aea243d9a2a
HanYu <hanyushome@gmail.com>
2018-11-16 01:55:59 +0800

    no message

--- Pending/TestGamble/memberIndex.js
+++ Pending/TestGamble/memberIndex.js
@@ -35,12 +35,36 @@ function clickTab(){
     dispatchMouseEvent($(document).find('a[href="load?lottery=PK10JSC&page=110"]')[0], 'click', true, true);
 }
 
-function fillfield(v){
+function fillfield(idx, v){
     // inject script to iframe https://jsfiddle.net/onury/ALHGP/
     var script = document.createElement( "script" )
     script.setAttribute('type', 'text/javascript');
-    script.innerHTML = '$(\'input[name="B1_1"]\').val('+v+')';
+    script.innerHTML = '$(\'input[name="B1_'+idx+'"]\').val('+v+')';
     $('iframe')[0].contentDocument.body.append(script)
+}
+
+function clickBet(){
+    //<input type="button" onclick="bet()" value="确定" class="button">
+    var script = document.createElement( "script" )
+    script.setAttribute('type', 'text/javascript');
+    script.innerHTML = '$(\'input[value="确定"]\').click()';
+    $('iframe')[0].contentDocument.body.append(script)
+}
+
+
+function clickConfirm(){
+    //<button type="button" class="ui-button ui-widget ui-state-default ui-corner-all ui-button-text-only" role="button"><span class="ui-button-text">确定</span></button>
+    // 以下5和2請用chrome檢查在console中下$('button[type="button"]')來看是哪一個
+    // 按掉錯誤提示
+    $('button[type="button"]')[5].click()
+    // 按下確認
+    $('button[type="button"]')[2].click()
+}
+
+function getDrawNum(){
+    // <span id="drawNumber">30859915</span>
+    var ret = $($('iframe')[0].contentDocument.body).find('#drawNumber').html()
+    return parseInt(ret)
 }
 
 function getSeconds(){
@@ -58,7 +82,21 @@ function getResult(){
     return parseInt(ret)
 }
 
+function getRank(){
+    var [before, after] = $("#result_balls").find("span").text().split("10")
+    var rank = []
+    for(var i=0; i<before.length; ++i){
+        rank.push(parseInt(before[i]))
+    }
+    rank.push(10)
+    for(var i=0; i<after.length; ++i){
+        rank.push(parseInt(after[i]))
+    }
+    return rank
+}
+
 (async ()=>{
+    
     console.log("close popup")
     closePopup()
     
@@ -67,27 +105,61 @@ function getResult(){
     clickTab()
     
     await delay(5000)
-    for(;;){
-        var second = getSeconds()
-        console.log(second)
-        if(second > 20){
-            console.log("fill field")
-            fillfield(1)
-        }
-        var result = getResult()
-        console.log(result)
-        await delay(1000)
+    
+    var startBet = 1
+    var record = {
+        state: "waitBuy",
+        result: getResult(),
+        drawNumber: getDrawNum(),
+        bet: startBet
     }
     
-    /*
-    await delay(1000)
-    console.log("click ok")
-    var script = document.createElement( "script" )
-    script.setAttribute('type', 'text/javascript');
-    script.innerHTML = '$(\'input[value="确定"]\').click()';
-    $('iframe')[0].contentDocument.body.append(script)
-    //<input type="button" onclick="bet()" value="确定" class="button">
-    */
+    for(;;){
+        var second = getSeconds()
+        console.log(record)
+        switch(record.state){
+        case "waitBuy":
+            {
+                if(second > 20 && second < 60){
+                    console.log("fill field")
+                    var pos = getRank()[0]
+                    fillfield(pos, record.bet)
+                    await delay(5000)
+                    /*
+                    console.log("Bet!!")
+                    clickBet()
+                    
+                    await delay(1000)
+                    clickConfirm()
+                    */
+                    record.state = "waitResult"
+                }
+            }
+            break
+        case "waitResult":
+            {
+                var currDrawNum = getDrawNum()
+                var enterNextDraw = currDrawNum > record.drawNumber
+                if(enterNextDraw == false){
+                    break
+                }
+                record.drawNumber = currDrawNum
+                await delay(5000)
+                
+                var result = getResult()
+                var isWin = result > record.result
+                if(isWin){
+                    record.bet = startBet
+                } else {
+                    record.bet = record.bet*2
+                }
+                record.result = result
+                record.state = "waitBuy"
+            }
+            break
+        }
+        await delay(1000)
+    }
 })()
 
 
