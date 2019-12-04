67fd5d498388a185d1f07d19d293cfb47ab1702a
HanYu1983 <hanyushome@gmail.com>
2019-12-04 09:20:59 +0800

    no message

--- robotTest/src/han/component/SaveComponent.java
+++ robotTest/src/han/component/SaveComponent.java
@@ -7,17 +7,27 @@ import java.io.Serializable;
 import java.util.HashMap;
 import java.util.Map;
 
-public abstract class SaveComponent implements IFileEvents {
+import robocode.BattleEndedEvent;
+import robocode.BulletHitBulletEvent;
+import robocode.BulletHitEvent;
+import robocode.BulletMissedEvent;
+import robocode.DeathEvent;
+import robocode.HitByBulletEvent;
+import robocode.HitRobotEvent;
+import robocode.HitWallEvent;
+import robocode.RobotDeathEvent;
+import robocode.RoundEndedEvent;
+import robocode.ScannedRobotEvent;
+import robocode.StatusEvent;
+import robocode.WinEvent;
+
+public abstract class SaveComponent implements IFileEvents, IBattleEvents {
 
 	protected abstract Serializable onNew();
 
 	protected abstract void onLoad(Object obj);
 
 	private Serializable saveObj;
-	
-	public void setSaveObjectManually(Serializable obj) {
-		this.saveObj = obj;
-	}
 
 	public Object getSaveObject() {
 		return saveObj;
@@ -30,8 +40,6 @@ public abstract class SaveComponent implements IFileEvents {
 			this.saveObj = obj;
 			onLoad(this.saveObj);
 		} catch (Exception e) {
-			// e.printStackTrace();
-			System.out.println("create new world");
 			Serializable obj = onNew();
 			this.saveObj = obj;
 		}
@@ -47,5 +55,97 @@ public abstract class SaveComponent implements IFileEvents {
 	@Override
 	public void onOutputStream(ObjectOutputStream oos) throws IOException {
 		oos.writeObject(saveObj);
+	}
+
+	@Override
+	public void onRoundStarted() {
+		if (this.getSaveObject() == null) {
+			Serializable obj = onNew();
+			this.saveObj = obj;
+			Map<String, Object> pool = new HashMap<>();
+			if (this.saveObj instanceof ISave) {
+				ISave save = (ISave) this.saveObj;
+				save.onRegister(pool);
+				save.onFindRef(pool);
+			}
+		}
+	}
+
+	@Override
+	public void onRoundEnded(RoundEndedEvent arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void onBattleEnded(BattleEndedEvent arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void onBulletHit(BulletHitEvent arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void onBulletHitBullet(BulletHitBulletEvent arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void onBulletMissed(BulletMissedEvent arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void onDeath(DeathEvent arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void onHitByBullet(HitByBulletEvent arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void onHitRobot(HitRobotEvent arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void onHitWall(HitWallEvent arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void onRobotDeath(RobotDeathEvent arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void onScannedRobot(ScannedRobotEvent arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void onStatus(StatusEvent arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void onWin(WinEvent arg0) {
+		// TODO Auto-generated method stub
+
 	}
 }
--- robotTest/src/han/demo/SaveFrameworkRobot.java
+++ robotTest/src/han/demo/SaveFrameworkRobot.java
@@ -1,7 +1,6 @@
 package han.demo;
 
 import java.io.Serializable;
-import java.util.HashMap;
 import java.util.Map;
 
 import org.jbox2d.common.Vec2;
@@ -9,25 +8,11 @@ import org.jbox2d.common.Vec2;
 import han.component.ComponentList;
 import han.component.ComponentRobot;
 import han.component.DrawCoord;
-import han.component.IBattleEvents;
 import han.component.SaveComponent;
 import han.component.action.ActionQueue;
 import han.component.action.ActionStack;
 import han.component.action.AltAction;
 import han.component.action.MoveTo;
-import robocode.BattleEndedEvent;
-import robocode.BulletHitBulletEvent;
-import robocode.BulletHitEvent;
-import robocode.BulletMissedEvent;
-import robocode.DeathEvent;
-import robocode.HitByBulletEvent;
-import robocode.HitRobotEvent;
-import robocode.HitWallEvent;
-import robocode.RobotDeathEvent;
-import robocode.RoundEndedEvent;
-import robocode.ScannedRobotEvent;
-import robocode.StatusEvent;
-import robocode.WinEvent;
 
 public class SaveFrameworkRobot extends ComponentRobot {
 
@@ -35,7 +20,7 @@ public class SaveFrameworkRobot extends ComponentRobot {
 		coms.addComponent(new Control());
 	}
 
-	private class Control extends SaveComponent implements Serializable, IBattleEvents {
+	private class Control extends SaveComponent implements Serializable {
 		/**
 		 * 
 		 */
@@ -43,10 +28,10 @@ public class SaveFrameworkRobot extends ComponentRobot {
 
 		@Override
 		protected Serializable onNew() {
+			System.out.println("onNew");
 			Main main = new Main(null);
 			main.robot = SaveFrameworkRobot.this;
 			SaveFrameworkRobot.this.coms.addComponent(main);
-			System.out.println("onNew");
 			return main;
 		}
 
@@ -57,99 +42,6 @@ public class SaveFrameworkRobot extends ComponentRobot {
 			main.robot = SaveFrameworkRobot.this;
 			SaveFrameworkRobot.this.coms.addComponent(main);
 		}
-
-		@Override
-		public void onRoundStarted() {
-			if (this.getSaveObject() == null) {
-				Main main = new Main(null);
-				main.robot = SaveFrameworkRobot.this;
-				SaveFrameworkRobot.this.coms.addComponent(main);
-				this.setSaveObjectManually(main);
-
-				Map<String, Object> pool = new HashMap<>();
-				main.onRegister(pool);
-				main.onFindRef(pool);
-			}
-		}
-
-		@Override
-		public void onRoundEnded(RoundEndedEvent arg0) {
-			// TODO Auto-generated method stub
-
-		}
-
-		@Override
-		public void onBattleEnded(BattleEndedEvent arg0) {
-			// TODO Auto-generated method stub
-
-		}
-
-		@Override
-		public void onBulletHit(BulletHitEvent arg0) {
-			// TODO Auto-generated method stub
-
-		}
-
-		@Override
-		public void onBulletHitBullet(BulletHitBulletEvent arg0) {
-			// TODO Auto-generated method stub
-
-		}
-
-		@Override
-		public void onBulletMissed(BulletMissedEvent arg0) {
-			// TODO Auto-generated method stub
-
-		}
-
-		@Override
-		public void onDeath(DeathEvent arg0) {
-			// TODO Auto-generated method stub
-
-		}
-
-		@Override
-		public void onHitByBullet(HitByBulletEvent arg0) {
-			// TODO Auto-generated method stub
-
-		}
-
-		@Override
-		public void onHitRobot(HitRobotEvent arg0) {
-			// TODO Auto-generated method stub
-
-		}
-
-		@Override
-		public void onHitWall(HitWallEvent arg0) {
-			// TODO Auto-generated method stub
-
-		}
-
-		@Override
-		public void onRobotDeath(RobotDeathEvent arg0) {
-			// TODO Auto-generated method stub
-
-		}
-
-		@Override
-		public void onScannedRobot(ScannedRobotEvent arg0) {
-			// TODO Auto-generated method stub
-
-		}
-
-		@Override
-		public void onStatus(StatusEvent arg0) {
-			// TODO Auto-generated method stub
-
-		}
-
-		@Override
-		public void onWin(WinEvent arg0) {
-			// TODO Auto-generated method stub
-
-		}
-
 	}
 
 	public static class Main extends ComponentList {
