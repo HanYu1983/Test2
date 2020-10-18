f26a4b5d8509f421a7fda4cad9c4a936e5e5187f
HanYu1983 <hanyushome@gmail.com>
2020-10-18 19:02:57 +0800

    no message

--- Pending/ClojureChromeExt/cljsDev/src/showK_chart.cljs
+++ Pending/ClojureChromeExt/cljsDev/src/showK_chart.cljs
@@ -1,7 +1,28 @@
-(ns showK-chart)
+(ns showK-chart
+  (:require [reagent.core :as r]))
 
-(defn main []  
-  (let [btn (js/document.createElement "button")
-        _ (set! (.-innerHTML btn) "CLICK ME")
-        _ (.appendChild js/document.body btn)])
-  (println "injected!!"))
+(def click-count (r/atom 0))
+
+(defn counting-component
+  []
+  [:div {:style {:position "absolute"
+                 :top 50
+                 :width 300
+                 :height 300
+                 :background-color "gray"}}
+   [:div {:style {:display "flex"}}
+    [:div {:style {:background-color "red"
+                   :flex 1}}]
+    "The state has a value: " @click-count
+    [:input {:type "button"
+             :value "Click me!"
+             :on-click #(swap! click-count inc)}]]])
+
+(defn root []
+  (counting-component))
+
+(defn main []
+  (let [rootDom (js/document.createElement "div")
+        _ (.appendChild js/document.body rootDom)
+        _ (r/render [root] rootDom)
+        _ (println "injected!!")]))
